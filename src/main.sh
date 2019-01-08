5cae5a9be18277cb9801b2ae85a750e4752ef813
Maciej Krüger <mkg20001@gmail.com>
2019-01-08 10:11:58 +0100

    feat: basic wiping concept, etc

--- src/main.sh
+++ src/main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# pkgs: jq parted lsblk
+
 if [ $(id -u) -gt 0 ]; then
   echo "ERROR: Must be run as root" >&2
   exit 2
@@ -63,10 +65,57 @@ log() {
 
 detect_root_location() {
   ROOT_MNT=$(awk -v needle="/" '$2==needle {print $1}' /proc/mounts)
-  ROOT_DEV=$(lsblk -no pkname "$ROOT_MNT")
-  ROOT_DEV="/dev/$ROOT_DEV"
+  ROOT_DEV_NAME=$(lsblk -no pkname "$ROOT_MNT")
+  ROOT_DEV="/dev/$ROOT_DEV_NAME"
 
-  log "System is mounted at $ROOT_MNT which is part of $ROOT_DEV"
+  log "System is mounted at $ROOT_MNT which is part of $ROOT_DEV, ignoring that disk"
+}
+
+get_from_list() {
+  head -n "$1" | tail -n 1
+}
+
+detect_disks() {
+  DEVS_J=$(lsblk -Jo name,rm,size,ro,type,mountpoint,hotplug,label,uuid,model,serial,vendor | jq '.blockdevices[] | select(.type == "disk" and .name != "'"$ROOT_DEV_NAME"'")')
+  DEVS=$(echo "$DEVS_J" | jq -r ".name")
+  DEVS=($DEVS)
+  DEV_NAME_LIST=$(echo "$DEVS_J" | jq -sr '.[] | .vendor + " " + .model + " " + .serial + " (" + .size + ")"')
+}
+
+contains () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+MAP="$TMP/.map"
+mkdir -p "$MAP"
+name_to_index() {
+  :
+}
+
+name_to_index_sync() {
+#  for dev in $DEVS; do
+#    for dev_index in $(ls "$MAP"); do
+  :
+}
+
+do_disk_wipe() {
+  DEV="$1"
+  DEV_NAME="$2"
+
+  log "Wiping $DEV with 000000 (1/3)..."
+  dd if=/dev/null "of=$DEV"
+  log "Wiping $DEV with 111111 (2/3)..."
+  dd if=/dev/null "of=$DEV" # TODO: use 0xffff
+  log "Wiping $DEV with random (3/3)..."
+  dd if=/dev/null "of=$DEV" # TODO: use random (but make it fast)
+
+  log "Creating msdos partition table on $DEV..."
+  yes | parted "$DEV" mktable msdos
+
+  log "Wiping completed for $DEV!"
 }
 
 render() {
